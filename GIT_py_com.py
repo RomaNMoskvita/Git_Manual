a6624d35562f0d9d680535badb215a6d73c25588
Roman Roman <RMoskvita@blumenbecker.pl>
2023-04-20 21:21:39 +0200

    New metod .append complete

--- GIT_py_com.py
+++ GIT_py_com.py
@@ -1,1 +1,76 @@
 print("This is Command for GIT")
+
+# Not Equals Operator 
+"""
+if "Yes" != "No":
+    # evaluates to True
+    print ("They are NOT equal")
+   
+    val1 = 10
+    val2 = 20 
+    
+    if val1 != val2:
+        print ("They are NOT equal")
+        
+    if (10>1) != (10>1000):
+        # True != False
+        print("They are NOT equal")
+
+#if Statement
+
+test_value = 100
+
+if test_value>1:
+    # Expression evaluates to True
+    print ("This code is executed!")
+    
+if test_value > 1000:
+    # Expression evaluates to False
+    print ("This code is NOT executed!")
+    
+print("Program continues at this point")
+
+primes = [2, 3, 5, 7, 11]
+items = ['cake', 'cookie', 'bread']
+total_items = items + ['biscuit', 'tart']
+print(total_items)
+
+empty_list = []
+
+"""
+#zmienne sterujace
+
+var1 = "ABB"
+var2 = "KUKA"
+var3 = "FANUC"
+
+sw1 = True
+sw2 = False
+
+val1 = 5
+val2 = 2.5
+val3 = 6.7
+val4 = 10
+
+#Sterowanie robotami 
+print (var1, var2)
+
+if sw1 == sw2:
+    print(val1)
+elif var2 =="KUKA":
+    print(val1, sw1)
+
+if (val1 == 6 or val2 == 2.5) and (not sw1):
+    print (var3)
+
+
+items =['cake','cookie', 'bread']
+print(items)
+
+orders = ['ABB','KUKA']
+orders.append('FANUC')
+print(orders)
+
+    
+    
+    
